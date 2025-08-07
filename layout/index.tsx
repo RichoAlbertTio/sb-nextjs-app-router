0a51eb8b6c36371082d846d354f4adbdce98beaf
RichoAlbertTio <study160702@gmail.com>
2025-08-08 03:31:58 +0700

    tugas hari-4

---
+++ layout/index.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+     
+      <div className={`flex flex-col min-h-screen`}>
+        <header className="bg-blue-600 text-white p-4">
+          <div className="cointainer mx-auto flex justify-between items-center">
+            <h1 className="text-xl font-bold">Richo Albert Tio</h1>
+            <ul className="flex space-x-4">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="/profile">Profile</Link>
+              </li>
+             
+            </ul>
+          </div>
+        </header>
+        <main className="flex-1 container mx-auto p-4">
+          {children}
+        </main>
+        <footer className="bg-gray-800 text-white p-4 text-center">
+          <p>&copy; {new Date().getFullYear()} My Personal Website</p>
+        </footer>
+
+        {/* cara akses style global */}
+        {/* style global ini di render sama misal header = header */}
+        {/* <div className="header">Header</div> */}
+        {/* cara akses style module */}
+        {/* cara akses id namun tidak di sarankan */}
+        {/* <div id={styles.main}>{children}</div> */}
+        {/* cara akses style module */}
+        {/* style module ini di render sama unique footer = akan du generate berbeda namanya */}
+        {/* <div className={styles.footer}>Footer</div> */}
+      </div>
+    </>
+  );
+}
